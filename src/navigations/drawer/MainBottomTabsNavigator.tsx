be2e34fd7486fee269c4f5046eecfe303510184f
olhkyle <kylekwon.dev@gmail.com>
2025-08-11 00:06:47 +0900

    feat : add title on Tab.Screen

--- src/navigations/drawer/MainBottomTabsNavigator.tsx
+++ src/navigations/drawer/MainBottomTabsNavigator.tsx
@@ -1,30 +1,18 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {HomeScreen, ProfileScreen, ShopScreen} from '../../screens/main';
+import { StyleSheet } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { HomeScreen, ProfileScreen, ShopScreen } from '../../screens/main';
 
 const Tab = createBottomTabNavigator();
 
 function MainBottomTabsNavigator() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{headerShown: false}}
-      />
-      <Tab.Screen
-        name="Shop"
-        component={ShopScreen}
-        options={{headerShown: false}}
-      />
-      <Tab.Screen
-        name="Profile"
-        component={ProfileScreen}
-        options={{headerShown: false}}
-      />
-    </Tab.Navigator>
-  );
+	return (
+		<Tab.Navigator>
+			<Tab.Screen name="Home" component={HomeScreen} options={{ title: 'FilmShack', headerShown: true }} />
+			<Tab.Screen name="Shop" component={ShopScreen} options={{ headerShown: false }} />
+			<Tab.Screen name="Profile" component={ProfileScreen} options={{ headerShown: false }} />
+		</Tab.Navigator>
+	);
 }
 
 const styles = StyleSheet.create({});
