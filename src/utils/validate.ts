d65c7b73640ea0dcfd03f3356e320e18e4e3f599
olhkyle <kylekwon.dev@gmail.com>
2025-08-11 00:05:09 +0900

    chore : change indent because of prettier

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,40 +1,38 @@
 interface ValidateTargetValues {
-  email: string;
-  password: string;
+	email: string;
+	password: string;
 }
 
 function validateUser(values: ValidateTargetValues) {
-  const errors = {
-    email: '',
-    password: '',
-  };
+	const errors = {
+		email: '',
+		password: '',
+	};
 
-  if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(values.email)) {
-    errors.email = '올바른 이메일 형식이 아닙니다.';
-  }
+	if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(values.email)) {
+		errors.email = '올바른 이메일 형식이 아닙니다.';
+	}
 
-  if (!(values.password.length >= 8 && values.password.length < 20)) {
-    errors.password = '비밀번호는 8-20자 사이로 입력해 주세요';
-  }
+	if (!(values.password.length >= 8 && values.password.length < 20)) {
+		errors.password = '비밀번호는 8-20자 사이로 입력해 주세요';
+	}
 
-  return errors;
+	return errors;
 }
 
 function validateLogin(values: ValidateTargetValues) {
-  return validateUser(values);
+	return validateUser(values);
 }
 
-function validateSignUp(
-  values: ValidateTargetValues & {passwordConfirm: string},
-) {
-  const errors = validateUser(values);
-  const signUpErrors = {...errors, passwordConfirm: ''};
+function validateSignUp(values: ValidateTargetValues & { passwordConfirm: string }) {
+	const errors = validateUser(values);
+	const signUpErrors = { ...errors, passwordConfirm: '' };
 
-  if (values.password !== values.passwordConfirm) {
-    signUpErrors.passwordConfirm = '비밀번호가 일치하지 않습니다';
-  }
+	if (values.password !== values.passwordConfirm) {
+		signUpErrors.passwordConfirm = '비밀번호가 일치하지 않습니다';
+	}
 
-  return signUpErrors;
+	return signUpErrors;
 }
 
-export {validateSignUp, validateLogin};
+export { validateSignUp, validateLogin };
