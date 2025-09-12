e9017c41c172cda0bb597b9d9c508b161dcffbd2
olhkyle <kylekwon.dev@gmail.com>
2025-09-12 13:43:47 +0900

    feat : add icon on bottomTabs

--- src/navigations/drawer/MainBottomTabsNavigator.tsx
+++ src/navigations/drawer/MainBottomTabsNavigator.tsx
@@ -1,16 +1,44 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { HomeScreen, ProfileScreen, ShopScreen } from '../../screens/main';
+import { HomeScreen, RecipeScreen, MeScreen } from '../../screens/main';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { colors } from '../../constants/globalStyle';
 
 const Tab = createBottomTabNavigator();
 
 function MainBottomTabsNavigator() {
 	return (
-		<Tab.Navigator>
-			<Tab.Screen name="Home" component={HomeScreen} options={{ title: 'FilmShack', headerShown: true }} />
-			<Tab.Screen name="Shop" component={ShopScreen} options={{ headerShown: false }} />
-			<Tab.Screen name="Profile" component={ProfileScreen} options={{ headerShown: false }} />
+		<Tab.Navigator
+			initialRouteName="Home"
+			screenOptions={{
+				animation: 'fade',
+				tabBarShowLabel: false,
+				tabBarActiveTintColor: colors.BLACK,
+				tabBarInactiveTintColor: colors.GREY[400], // 비활성 탭 색상
+			}}>
+			<Tab.Screen
+				name="Home"
+				component={HomeScreen}
+				options={{
+					title: 'FilmShack',
+					headerShown: true,
+					tabBarIcon: ({ color }) => <Ionicons name="home" color={color} size={24} />,
+				}}
+			/>
+			<Tab.Screen
+				name="Recipe"
+				component={RecipeScreen}
+				options={{
+					headerShown: false,
+					tabBarIcon: ({ color }) => <Ionicons name="camera" color={color} size={30} />,
+				}}
+			/>
+			<Tab.Screen
+				name="Me"
+				component={MeScreen}
+				options={{ headerShown: false, tabBarIcon: ({ color }) => <Ionicons name="person" color={color} size={24} /> }}
+			/>
 		</Tab.Navigator>
 	);
 }
