b91ba4162a2ea9f4b3d0c9529f5b0d01894a661b
olhkyle <kylekwon.dev@gmail.com>
2025-09-12 13:43:30 +0900

    feat : add react query options on Root Component

--- App.tsx
+++ App.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './src/navigations/root/RootNavigator';
+import { AppStateStatus, Platform } from 'react-native';
+import { QueryClient, QueryClientProvider, focusManager } from '@tanstack/react-query';
+import useOnlineManager from './src/hooks/useOnlineManager';
+import useAppState from './src/hooks/useAppState';
+
+function onAppStateChange(status: AppStateStatus) {
+	if (Platform.OS !== 'web') {
+		focusManager.setFocused(status === 'active');
+	}
+}
+
+const queryClient = new QueryClient({
+	defaultOptions: { queries: { retry: 2 } },
+});
 
 function App() {
-  return (
-    <NavigationContainer>
-      <RootNavigator />
-    </NavigationContainer>
-  );
+	useOnlineManager();
+	useAppState(onAppStateChange);
+
+	return (
+		<QueryClientProvider client={queryClient}>
+			<NavigationContainer>
+				<RootNavigator />
+			</NavigationContainer>
+		</QueryClientProvider>
+	);
 }
 
 export default App;
