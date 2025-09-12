c9d36741e00b47a1e375bbe91fa2c64585dceb37
olhkyle <kylekwon.dev@gmail.com>
2025-09-12 13:40:44 +0900

    feat : change Screen names

---
+++ src/screens/main/MeScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, SafeAreaView } from 'react-native';
+
+function MeScreen() {
+	return (
+		<SafeAreaView>
+			<Text>Profile</Text>
+		</SafeAreaView>
+	);
+}
+
+const styles = StyleSheet.create({});
+
+export default MeScreen;
---
+++ src/screens/main/RecipeScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, SafeAreaView } from 'react-native';
+
+function RecipeScreen() {
+	return (
+		<SafeAreaView>
+			<Text>Purchase</Text>
+		</SafeAreaView>
+	);
+}
+
+const styles = StyleSheet.create({});
+
+export default RecipeScreen;
